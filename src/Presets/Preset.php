373d4b1a07b32222b93363ed2ca4c4ee83db08ea
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2020-01-28 19:45:02 -0500

    Update Preset.php

--- src/Presets/Preset.php
+++ src/Presets/Preset.php
@@ -80,6 +80,7 @@ class Preset extends LaravelUiPreset
     protected static function updateSass()
     {
         copy(__DIR__ . '/stubs/_variables.scss.stub', resource_path('sass/_variables.scss'));
+        copy(__DIR__ . '/stubs/_sidebar-layout.scss.stub', resource_path('sass/_sidebar-layout.scss'));
         copy(__DIR__ . '/stubs/bootstrap.scss.stub', resource_path('sass/bootstrap.scss'));
         copy(__DIR__ . '/stubs/materialize.scss.stub', resource_path('sass/materialize.scss'));
     }
